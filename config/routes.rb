0eab674016718bab2781dccb1172e5e1dcdbe38d
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-04-06 10:35:21 +0000

    [Add]4-10 allow to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'homes#top'
+  devise_for :users
 
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 
   get 'homes/about',to: 'homes#about',as: 'about'
 end
